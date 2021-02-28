d462c548ff21b7e9af3503ca7f298c8ccce38139
slama34849 <suryarajlama@gmail.com>
2021-02-27 22:02:37 -0500

    Adding Terraform code to get Amazon Linux AMI

---
+++ get-ami/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+output "ami" {
+    value = data.aws_ami.app_ami.id
+}
